39212e65b6cfbff64fd1e68c8b1b8d866a13df08
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-05-07 12:49:28 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var encrypt = 
+var encrypt =  
 {
-  simple: function(string) 
+  simple: function() 
   {
     var newstr;
     //for(let i = 1; i<27; i++)
