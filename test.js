2c963c67a5b8211424fe28792f39fa0caa37b225
Mason Walker <46076583+cubesniper24@users.noreply.github.com>
2021-05-07 12:55:59 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var encrypt =  
 {
-  simple: function() 
+  simple: function(string) 
   {
-    alert("test");
+    var newstr;
+    for(let i = 1; i<27; i++)
+    {
+     newstr = string.replace(letters[i-1], "a");
+     }
+    alert(newstr);
   }
 };
